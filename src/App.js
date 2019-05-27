ac9a56a5afd4501c34a45dc8fe0ed8cdafb5a197
chetan <chetanmajithiya28@gmail.com>
2019-05-27 15:40:43 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch } from "react-router-dom";
+import MovieList from './components/movie-list'
+import Error from './components/error'
+import MovieDetail from './components/movie-detail'
+import Search from './components/search'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+        <div>
+          <BrowserRouter>
+            <Switch>
+              <Route path="/" exact component={MovieList} />
+              <Route path="/movie-detail/:id" component={MovieDetail} />
+              <Route path="/search/:search" component={Search} />
+              <Route component={Error} />
+            </Switch>
+          </BrowserRouter>
+          <footer className="ps-Clear-Fix" style={{display: 'block', height: '100px', width: '100%', backgroundColor: '#333545', marginTop: '20px'}}>
+          </footer>
+        </div>
+    );
+  }
 }
 
 export default App;
